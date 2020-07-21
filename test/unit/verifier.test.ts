26eb2f7536add0654443995ba40d7332bf01e61c
WMXPY <wm@mengw.io>
2020-07-21 16:00:29 -0500

    implement verifier unit test, publish

--- test/unit/verifier.test.ts
+++ test/unit/verifier.test.ts
@@ -11,13 +11,38 @@ import { DataVerifier } from "../../src";
 
 describe('Given {DataVerifier} Class', (): void => {
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const chance: Chance.Chance = new Chance('processor-verifier');
 
     it('should be able to construct', (): void => {
 
-        const api: DataVerifier = DataVerifier.create();
+        const verifier: DataVerifier = DataVerifier.create();
 
-        expect(api).to.be.instanceOf(DataVerifier);
+        expect(verifier).to.be.instanceOf(DataVerifier);
+    });
+
+    it('should be able valid data - happy path', (): void => {
+
+        const value: string = chance.string();
+
+        const verifier: DataVerifier = DataVerifier.create();
+
+        verifier.add((original: string) => original === value);
+
+        expect(verifier).to.be.lengthOf(1);
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        expect(verifier.verify(value)).to.be.true;
+    });
+
+    it('should be able valid data - sad path', (): void => {
+
+        const value: string = chance.string();
+
+        const verifier: DataVerifier = DataVerifier.create();
+
+        verifier.add((original: string) => original !== value);
+
+        expect(verifier).to.be.lengthOf(1);
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        expect(verifier.verify(value)).to.be.false;
     });
 });
