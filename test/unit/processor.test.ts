5ab8139405dba993a21a0993ab17369f09fe03d1
WMXPY <wm@mengw.io>
2020-07-21 15:58:44 -0500

    implement processor unit tests

--- test/unit/processor.test.ts
+++ test/unit/processor.test.ts
@@ -11,13 +11,35 @@ import { DataProcessor } from "../../src";
 
 describe('Given {DataProcessor} Class', (): void => {
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const chance: Chance.Chance = new Chance('processor-processor');
 
     it('should be able to construct', (): void => {
 
-        const api: DataProcessor = DataProcessor.create();
+        const processor: DataProcessor = DataProcessor.create();
 
-        expect(api).to.be.instanceOf(DataProcessor);
+        expect(processor).to.be.instanceOf(DataProcessor);
+    });
+
+    it('should be able to constraint data', (): void => {
+
+        const value: string = chance.string();
+
+        const processor: DataProcessor = DataProcessor.create();
+
+        expect(processor).to.be.lengthOf(0);
+        expect(processor.process(value)).to.be.equal(value);
+    });
+
+    it('should be able to change data', (): void => {
+
+        const value: string = chance.string();
+        const result: string = chance.string();
+
+        const processor: DataProcessor = DataProcessor.create();
+
+        processor.add(() => result);
+
+        expect(processor).to.be.lengthOf(1);
+        expect(processor.process(value)).to.be.equal(result);
     });
 });
