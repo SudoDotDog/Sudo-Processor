c9210fd6b024c2e1dac431e94bb7f1e548c358fc
WMXPY <wm@mengw.io>
2020-10-12 12:52:56 -0500

    finish unit test, realase

--- src/async/hook.ts
+++ src/async/hook.ts
@@ -5,6 +5,7 @@
  */
 
 import { AsyncDataProcessor } from "./processor";
+import { AsyncSideEffect } from "./side-effect";
 import { AsyncDataVerifier } from "./verifier";
 
 export class AsyncDataHook<T extends any = any> {
@@ -14,23 +15,34 @@ export class AsyncDataHook<T extends any = any> {
         return new AsyncDataHook<T>();
     }
 
+    private readonly _sideEffect: AsyncSideEffect<T>;
     private readonly _processor: AsyncDataProcessor<T>;
     private readonly _verifier: AsyncDataVerifier<T>;
 
     private constructor(
+        sideEffect: AsyncSideEffect<T> = AsyncSideEffect.create<T>(),
         processor: AsyncDataProcessor<T> = AsyncDataProcessor.create<T>(),
         verifier: AsyncDataVerifier<T> = AsyncDataVerifier.create<T>(),
     ) {
 
+        this._sideEffect = sideEffect;
         this._processor = processor;
         this._verifier = verifier;
     }
 
+    public get sideEffect(): AsyncSideEffect<T> {
+        return this._sideEffect;
+    }
     public get processor(): AsyncDataProcessor<T> {
         return this._processor;
     }
     public get verifier(): AsyncDataVerifier<T> {
         return this._verifier;
+    }
+
+    public async execute(data: T): Promise<void> {
+
+        return await this._sideEffect.execute(data);
     }
 
     public async process(data: T): Promise<T> {
@@ -45,6 +57,7 @@ export class AsyncDataHook<T extends any = any> {
 
     public clear(): this {
 
+        this._sideEffect.clear();
         this._processor.clear();
         this._verifier.clear();
         return this;
@@ -53,6 +66,7 @@ export class AsyncDataHook<T extends any = any> {
     public clone(): AsyncDataHook<T> {
 
         const hook: AsyncDataHook<T> = new AsyncDataHook<T>(
+            this._sideEffect.clone(),
             this._processor.clone(),
             this._verifier.clone(),
         );
--- src/sync/hook.ts
+++ src/sync/hook.ts
@@ -5,6 +5,7 @@
  */
 
 import { DataProcessor } from "./processor";
+import { SideEffect } from "./side-effect";
 import { DataVerifier } from "./verifier";
 
 export class DataHook<T extends any = any> {
@@ -14,23 +15,34 @@ export class DataHook<T extends any = any> {
         return new DataHook<T>();
     }
 
+    private readonly _sideEffect: SideEffect<T>;
     private readonly _processor: DataProcessor<T>;
     private readonly _verifier: DataVerifier<T>;
 
     private constructor(
+        sideEffect: SideEffect<T> = SideEffect.create<T>(),
         processor: DataProcessor<T> = DataProcessor.create<T>(),
         verifier: DataVerifier<T> = DataVerifier.create<T>(),
     ) {
 
+        this._sideEffect = sideEffect;
         this._processor = processor;
         this._verifier = verifier;
     }
 
+    public get sideEffect(): SideEffect<T> {
+        return this._sideEffect;
+    }
     public get processor(): DataProcessor<T> {
         return this._processor;
     }
     public get verifier(): DataVerifier<T> {
         return this._verifier;
+    }
+
+    public execute(data: T): void {
+
+        return this._sideEffect.execute(data);
     }
 
     public process(data: T): T {
@@ -45,6 +57,7 @@ export class DataHook<T extends any = any> {
 
     public clear(): this {
 
+        this._sideEffect.clear();
         this._processor.clear();
         this._verifier.clear();
         return this;
@@ -53,6 +66,7 @@ export class DataHook<T extends any = any> {
     public clone(): DataHook<T> {
 
         const hook: DataHook<T> = new DataHook<T>(
+            this._sideEffect.clone(),
             this._processor.clone(),
             this._verifier.clone(),
         );
